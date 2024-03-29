075fcbdfe0d7bebfd92fe2a6bf3022711987e345
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-03-28 22:31:53 -0700

    put api to mongoDB

--- server.js
+++ server.js
@@ -2,21 +2,55 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const Product = require("./models/product.model");
+
 const app = express();
 const port = 3001;
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/user", (req, res) => {
-  res.send("Alberto Sierra");
-});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+    if (!product)
+      return res
+        .status(404)
+        .json("The product with the given ID was not found");
+
+    const updatedProduct = await Product.findById(req.params.id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
